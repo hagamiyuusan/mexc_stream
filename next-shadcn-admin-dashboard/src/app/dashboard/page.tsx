f945667b44a6ebed273ac93cb6b95d147817386e
Huy <vinhhuyqna@gmail.com>
2024-11-16 10:51:22 +0700

    update command

--- next-shadcn-admin-dashboard/src/app/dashboard/page.tsx
+++ next-shadcn-admin-dashboard/src/app/dashboard/page.tsx
@@ -29,7 +29,7 @@ export default function Page() {
   const [balances, setBalances] = useState<Balance[]>([]);
 
   useEffect(() => {
-    const ws = new WebSocket(`${process.env.NEXT_PUBLIC_WS_URL}/ws`);
+    const ws = new WebSocket(`ws://localhost:8000/ws`);
     console.log(process.env.NEXT_PUBLIC_WS_URL);
     ws.onmessage = (event) => {
       console.log(event.data);
